65868f00192af2fac3764f8368331ed59cd2fdf1
Stephen <stephen.fuhrmann@web.de>
2011-08-29 15:35:01 +0200

    Neuanfang

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvdverleih::Application.config.secret_token = '19215bbe1c28efef9c6f1a1b8e2a852ea9d080f14e76ab0e394f7fccdef3155e7553806fa619843b18ce3461bd4ded2c6e89f746245e8dd06f8df9a5715bb644'
